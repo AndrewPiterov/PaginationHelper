2bb91ec8fef23d03c361eabfeb9d83b0667a9236
Andrey Piterov <piterov1990@gmail.com>
2016-02-11 09:05:48 +0300

    First commit.

---
+++ PaginationHelper.Test/PaginationHelperTests.cs
@@ -0,0 +1,72 @@
+﻿using NUnit.Framework;
+
+namespace PaginationHelper.Test
+{
+    [TestFixture]
+    public class PaginationHelperTests
+    {
+        private PaginationHelper _phelper;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _phelper = new PaginationHelper(new[] { "a", "b", "c", "d", "e", "f" }, 4);
+        }
+
+        [Test]
+        public void ReturnPageCount()
+        {
+            Assert.AreEqual(2, _phelper.PageCount());
+        }
+
+        [Test]
+        public void ItemCount()
+        {
+            var arr = new[] {"a", "b" };
+            var ph = new PaginationHelper(arr, 1);
+            Assert.AreEqual(arr.Length, ph.ItemCount());
+        }
+
+        [Test]
+        public void PageItemCount()
+        {
+            Assert.AreEqual(4, _phelper.PageItemCount(0));
+        }
+
+        [Test]
+        public void TakeSecondPageItems()
+        {
+            Assert.AreEqual(2, _phelper.PageItemCount(1));
+        }
+
+        [Test]
+        public void TakeNonexistpage()
+        {
+            Assert.AreEqual(-1, _phelper.PageItemCount(2));
+        }
+
+        [Test]
+        public void ElementOnSeconPage()
+        {
+            Assert.AreEqual(1, _phelper.PageIndex(5));
+        }
+
+        [Test]
+        public void ElementOnFirstPage()
+        {
+            Assert.AreEqual(0, _phelper.PageIndex(2));
+        }
+
+        [Test]
+        public void ElementOnNonExistPage()
+        {
+            Assert.AreEqual(-1, _phelper.PageIndex(20));
+        }
+
+        [Test]
+        public void ElementOnPageWithNegativeIndex()
+        {
+            Assert.AreEqual(-1, _phelper.PageIndex(-10));
+        }
+    }
+}
---
+++ PaginationHelper/PaginationHelper.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PaginationHelper
+{
+    public class PaginationHelper
+    {
+        private readonly string[] _array;
+        private readonly int _perPage;
+
+        public PaginationHelper(IEnumerable<string> array, int perPaage)
+        {
+            _array = array.ToArray();
+            _perPage = perPaage;
+        }
+
+        /// <summary>
+        /// количество элементов
+        /// </summary>
+        /// <returns></returns>
+        public int PageCount() => (int)Math.Ceiling((decimal)_array.Length / _perPage);
+
+        /// <summary>
+        /// количество страниц
+        /// </summary>
+        /// <returns></returns>
+        public int ItemCount() => _array.Length;
+
+        /// <summary>
+        /// количество элементов на странице с указанным номером
+        /// </summary>
+        /// <param name="pageNumber"></param>
+        /// <returns></returns>
+        public int PageItemCount(int pageNumber) =>
+            IsPageExist(pageNumber) ? _array.Skip(pageNumber * _perPage).Take(_perPage).Count() : -1;
+
+        /// <summary>
+        /// номер страницы, на которой находится элемент с указанным номером
+        /// </summary>
+        /// <param name="itemIndex"></param>
+        /// <returns></returns>
+        public int PageIndex(int itemIndex)
+        {
+            if (itemIndex < 0 || itemIndex > ItemCount())
+                return -1;
+
+            var full = itemIndex / _perPage;
+            var rem = itemIndex % _perPage;
+
+            if (rem > 0)
+                return full;
+
+            return full - 1;
+        }
+
+
+        private bool IsPageExist(int pageNumber) => PageCount() - 1 >= pageNumber;
+    }
+}
